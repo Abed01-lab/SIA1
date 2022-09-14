3cdca81169349a3f7f45121dcbf048edbcf79f09
Abed01-lab <abedharirii@gmail.com>
2022-09-14 23:27:14 +0200

    Added soap API

--- soap.ts
+++ soap.ts
@@ -1,1 +1,45 @@
+import { listen } from "soap";
+import express from "express";
+import bodyParser from "body-parser";
 
+interface Person {
+  name: String;
+  ip: String;
+  mail: String;
+}
+
+interface IGetName {
+  Person: Person[];
+}
+
+const app = express();
+const myService = {
+  NameService: {
+    NameServiceSOAP: {
+      GetName: function ({ Person }: IGetName) {
+        console.log(Person);
+
+        const People = Person.map((person: any) => {
+          person["gender"] = "male";
+          return person;
+        });
+        return { People };
+      },
+    },
+  },
+};
+const xml = require("fs").readFileSync("soap.wsdl", "utf8");
+app.use(
+  bodyParser.raw({
+    type: function () {
+      return true;
+    },
+    limit: "5mb",
+  })
+);
+app.listen(8001, function () {
+  //Note: /wsdl route will be handled by soap module
+  //and all other routes & middleware will continue to work
+  listen(app, "/wsdl", myService, xml);
+  console.log("Server started");
+});
